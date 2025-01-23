20e607aeac22a68c45aeeb3330c0f15c4632a47a
shafie-isak <shafiewisak@gmail.com>
2025-01-23 22:54:22 +0300

    resoving things

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,12 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import userRoute from "./routes/user.js";
-import incidentRouter from "./routes/incident.js";
-import assignedRouter from "./routes/assignedIncident.js";
-import cors from 'cors';
-import { authenticate, authorize } from './middleware/auth.js';
-import bodyParser from "body-parser";
-
-dotenv.config();
-
-
+import express from 'express';
 const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.json());
 
-app.use("/api/users", userRoute);
-app.use("/api/incident", incidentRouter);
-app.use("/api/incident/assign", assignedRouter);
+const PORT = 5000;
 
-const port = 5000;
-connectDB();
+app.get("/", (req, res) => {
+    res.send("Hello, From server");
+})
 
-
-app.listen(port, ()=> {
-    console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 });
